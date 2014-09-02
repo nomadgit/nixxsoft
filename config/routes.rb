88ae6a1bcf64f9212e7a2f90bd721f53b33c081f
nixxfazlee <nikfadzlie@gmail.com>
2014-09-02 21:01:07 +0800

    pagebru

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  get 'pages/browse'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
